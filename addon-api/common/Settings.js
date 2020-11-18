5610ea7714dd18105d68290f451fed9df286c50a
GrahamSH-LLK <GrahamSH-LLK@users.noreply.github.com>
2020-11-18 16:14:31 +0000

    style: format code

--- addon-api/common/Settings.js
+++ addon-api/common/Settings.js
@@ -4,18 +4,18 @@ export default class Settings extends EventTarget {
     this._addonId = addonObject.self.id;
     scratchAddons.eventTargets.settings.push(this);
   }
-    /**
-     * @todo Write the documentation.
-     */
+  /**
+   * @todo Write the documentation.
+   */
   get(optionName) {
     const settingsObj = scratchAddons.globalState.addonSettings[this._addonId] || {};
     const value = settingsObj[optionName];
     if (value === undefined) throw "ScratchAddons exception: invalid setting ID";
     else return value;
   }
-   /**
-    * @todo Write the documentation.
-    */
+  /**
+   * @todo Write the documentation.
+   */
   _removeEventListeners() {
     scratchAddons.eventTargets.settings.splice(
       scratchAddons.eventTargets.settings.findIndex((x) => x === this),
