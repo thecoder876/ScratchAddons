b96e69b273f08d9b40cd278518a0c5bbd27b0d8d
GrahamSH <64214252+GrahamSH-LLK@users.noreply.github.com>
2020-11-18 11:18:40 -0500

    Update Badge.js

--- addon-api/background/Badge.js
+++ addon-api/background/Badge.js
@@ -8,16 +8,28 @@ export default class Badge {
       color: null,
     };
   }
+  /**
+   * @todo Write the documentation.
+   */
   get text() {
     return this._text;
   }
+  /**
+   * @todo Write the documentation.
+   */
   get color() {
     return this._color;
   }
+  /**
+   * @todo Write the documentation.
+   */
   set text(val) {
     this._text = val;
     scratchAddons.localState.badges[this._addonId].text = val;
   }
+  /**
+   * @todo Write the documentation.
+   */
   set color(val) {
     this._color = val;
     scratchAddons.localState.badges[this._addonId].color = val;
