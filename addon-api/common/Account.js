73c74e4dc21b051cbd5725c1cf5828b17c7232df
GrahamSH <64214252+GrahamSH-LLK@users.noreply.github.com>
2020-11-18 11:12:23 -0500

    Update Account.js

--- addon-api/common/Account.js
+++ addon-api/common/Account.js
@@ -1,13 +1,25 @@
 export default class Account extends EventTarget {
+  /**
+   * @todo Write the documentation.
+   */
   constructor() {
     super();
   }
+  /**
+   * @todo Write the documentation.
+   */
   getMsgCount() {
     return scratchAddons.methods.getMsgCount();
   }
+  /**
+   * @todo Write the documentation.
+   */
   getMessages(...args) {
     return scratchAddons.methods.getMessages(...args);
   }
+  /**
+   * @todo Write the documentation.
+   */
   clearMessages() {
     return scratchAddons.methods.clearMessages();
   }
