fb3000728d828effbb43ed69e72acf17c52772fb
GrahamSH <64214252+GrahamSH-LLK@users.noreply.github.com>
2020-11-19 18:07:58 -0500

    Update fetch.js

--- addon-api/common/fetch.js
+++ addon-api/common/fetch.js
@@ -1,3 +1,11 @@
+/**
+ * Fetch
+ * addon.fetch API
+ * @export
+ * @param {*} url
+ * @param {*} [opts={}]
+ * @return {*} 
+ */
 export default function fetch(url, opts = {}) {
   if (!opts.headers) opts.headers = {};
   opts.headers["X-ScratchAddons-Uses-Fetch"] = "true";
