ae2b82b1f8425ad7e8d8f5d4ab5af448e8b15769
GrahamSH <64214252+GrahamSH-LLK@users.noreply.github.com>
2020-11-19 09:57:47 -0500

    Update Auth.js

--- addon-api/common/Auth.js
+++ addon-api/common/Auth.js
@@ -4,38 +4,37 @@ export default class Auth extends EventTarget {
     scratchAddons.eventTargets.auth.push(this);
   }
   /**
-   * @todo Write the documentation.
-   * @category Category
+   * @return {boolean} Whether the user is logged in or not
    */
   get isLoggedIn() {
     return scratchAddons.globalState.auth.isLoggedIn;
   }
   /**
-   * @todo Write the documentation.
+   * @return {string} The username of the currently logged in user
    */
   get username() {
     return scratchAddons.globalState.auth.username;
   }
   /**
-   * @todo Write the documentation.
+   * @return {string} The user ID of the currently logged in user.
    */
   get userId() {
     return scratchAddons.globalState.auth.userId;
   }
   /**
-   * @todo Write the documentation.
+   * @return {string} The value of the X-Token header used in the Scratch REST API.
    */
   get xToken() {
     return scratchAddons.globalState.auth.xToken;
   }
   /**
-   * @todo Write the documentation.
+   * @return {string} The value of the scratchcsrftoken cookie.
    */
   get csrfToken() {
     return scratchAddons.globalState.auth.csrfToken;
   }
   /**
-   * @todo Write the documentation.
+   * @return {string} The language code for the language choice the user has made in Scratch, for example. "en".
    */
   get scratchLang() {
     return scratchAddons.globalState.auth.scratchLang;
